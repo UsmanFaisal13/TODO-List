f0d5c65ece8611d16d5b41e7ab420a2946b7dbf8
UsmanFaisal13 <130107471+UsmanFaisal13@users.noreply.github.com>
2024-11-23 02:10:54 +0500

    all mandatory features added

---
+++ app/components/Submit.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { useState, FormEvent, ChangeEvent } from "react";
+
+interface Task {
+    id: string;
+    title: string;
+    completed: boolean;
+}
+
+
+export default function Submit() {
+    const [task, setTask] = useState<string>("");
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (task !== "") {
+            setTasks((tasks) => [
+                ...tasks,
+                {
+                    id: crypto.randomUUID(),
+                    title: task,
+                    completed: false
+                }
+            ]);
+
+            setTask("");
+        }
+        console.log(tasks)
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTask(e.target.value);
+    };
+
+    return (
+        <>
+            <form className="flex gap-2" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={task}
+                    onChange={handleChange}
+                    placeholder="Type your task"
+                    className="rounded-md px-4 py-2 bg-zinc-900 " required
+
+                />
+                <input
+                    type="submit"
+                    value="Add Task"
+                    className="px-4 py-2 rounded-md bg-blue-300 hover:bg-blue-200 transition-all"
+                />
+
+
+            </form>
+            <ul className="flex flex-col gap-2 items-center">
+                {
+                    tasks.map((task) => {
+                        return (
+                            <li key={task.id} className="bg-zinc-900 rounded-md px-4 py-2 flex justify-between items-center lg:w-80 w-72 h-fit">
+                                <h1 className="px-4 break-words w-56">{task.title}</h1>
+                                <input type="checkbox" />
+
+
+                            </li>
+                        )
+                    })
+                }
+
+            </ul>
+        </>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Submit from "./components/Submit";
+export default function Home() {
+  return (
+
+    <div className="flex flex-col h-screen w-full items-center py-16 overflow-x-hidden lg:gap-8 gap-6">
+      <h1 className="lg:text-6xl text-5xl font-Darker_Grotesque">TO-DO List</h1>
+      <Submit />
+
+
+
+
+    </div>
+  );
+}
